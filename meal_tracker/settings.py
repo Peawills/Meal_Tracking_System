dc1f414150e37b65380ed17e18b5a83abcdb5f0c
Peawills <115140417+Peawills@users.noreply.github.com>
2025-07-07 13:30:02 +0100

    Deployment

--- meal_tracker/settings.py
+++ meal_tracker/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'meal_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'meal_tracker_db',  # use the DB name you created
+        'USER': 'postgres',         # or 'meal_user' if you created one
+        'PASSWORD': 'olayemiolaa1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
